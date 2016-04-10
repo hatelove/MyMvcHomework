2b7bf558caa5b7e72ec94f8ec10fec3c369b2e11
hatelove <jay.hatelove@gmail.com>
2016-04-10 14:38:25 +0800

    add displayFormat for Date and Amount

--- MyMoney/MyMoney/Models/ViewModels/AccountingViewModel.cs
+++ MyMoney/MyMoney/Models/ViewModels/AccountingViewModel.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using MyMoney.Models.Enums;
+﻿using MyMoney.Models.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyMoney.Models.ViewModels
 {
     public class AccountingViewModel
     {
+        //[DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString ="{0:N0}")]
         public int Amount { get; internal set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime Date { get; internal set; }
+
         public string Remark { get; internal set; }
         public AccountingType Type { get; internal set; }
     }
