c85ae959c791617dc55d2f63256e57e73abab7d1
hatelove <jay.hatelove@gmail.com>
2016-04-10 11:18:45 +0800

    homework initialized

---
+++ MyMoney/MyMoney/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMoney.Startup))]
+namespace MyMoney
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
