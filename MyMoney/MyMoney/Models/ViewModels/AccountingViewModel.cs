8b3a5665e9831e6ba05739ab88d3b45409f4f4a0
hatelove <jay.hatelove@gmail.com>
2016-04-23 21:43:36 +0800

    add [required] on ViewModel

--- MyMoney/MyMoney/Models/ViewModels/AccountingViewModel.cs
+++ MyMoney/MyMoney/Models/ViewModels/AccountingViewModel.cs
@@ -8,17 +8,21 @@ namespace MyMoney.Models.ViewModels
     {
         [Display(Name = "金額")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
+        [Required]
         public int Amount { get; set; }
 
         [Display(Name = "日期")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Required]
         public DateTime Date { get; set; }
 
         [Display(Name = "備註")]
+        [Required]
         public string Remark { get; set; }
 
         [Display(Name = "類別")]
+        [Required]
         public AccountingType Type { get; set; }
     }
 }
