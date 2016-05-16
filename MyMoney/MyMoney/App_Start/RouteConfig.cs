05e1bad8639c76c241c5748d7d097ab5c25186b1
hatelove <jay.hatelove@gmail.com>
2016-05-16 14:23:02 +0800

    add 記帳本 route setting

--- MyMoney/MyMoney/App_Start/RouteConfig.cs
+++ MyMoney/MyMoney/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MyMoney
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Accouting",
+                url: "skilltree",
+                defaults: new { controller = "Accounting", action = "Add" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
